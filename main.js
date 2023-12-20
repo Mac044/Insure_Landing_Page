f5cdd21861e3281375a6981c84f695999cf4b0ea
Mac044 <Olasubomiagbomeji@gmail.com>
2023-12-20 12:34:24 -0800

    Done with narBar

---
+++ main.js
@@ -0,0 +1,8 @@
+let toggleNavStatus = false;
+
+
+let toggleNav = function() {
+    let getNavlist = document.getElementById("Navlist")
+    let getNavlistUl = document.querySelector(".Nav_list ul")
+    let getNavlist2 = document.querySelector(".Nav_list .Navlist")
+}
